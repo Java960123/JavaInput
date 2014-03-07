5440fa8d4bcd3ab9905549b067ade202adbb0ed8
Java960123 <648901234@qq.com>
2014-03-07 09:42:35 -0500

    Create ReadCharByChar.java

---
+++ ReadCharByChar/src/ReadCharByChar.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class ReadCharByChar {
+
+	ReadCharByChar(){
+		
+	}
+	
+	public static void main(String [ ] args) throws IOException{		
+		char[] myChar = null;
+		Scanner s = null;
+        try {
+            s = new Scanner(new BufferedReader(new FileReader("C:\\Users\\src\\input.txt")));
+            while (s.hasNext())
+            {
+               String str = s.next(); 
+               myChar = str.toCharArray();
+            }
+        } finally {
+            if (s != null) {
+                s.close();
+            }
+        }
+        for(int i = 0; i < 20; i++){
+			System.out.println(myChar[i]);
+        }
+	}
+}
