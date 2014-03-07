38a1aeb22572dfda7401b2e4d85cfd231628c8e2
Java960123 <648901234@qq.com>
2014-03-07 09:44:09 -0500

    Update ReadCharByChar.java

--- ReadCharByChar/src/ReadCharByChar.java
+++ ReadCharByChar/src/ReadCharByChar.java
@@ -14,20 +14,20 @@ public class ReadCharByChar {
 	public static void main(String [ ] args) throws IOException{		
 		char[] myChar = null;
 		Scanner s = null;
-        try {
-            s = new Scanner(new BufferedReader(new FileReader("C:\\Users\\src\\input.txt")));
-            while (s.hasNext())
-            {
-               String str = s.next(); 
-               myChar = str.toCharArray();
-            }
-        } finally {
-            if (s != null) {
-                s.close();
-            }
-        }
-        for(int i = 0; i < 20; i++){
-			System.out.println(myChar[i]);
-        }
+	        try {
+	            s = new Scanner(new BufferedReader(new FileReader("C:\\Users\\src\\input.txt")));
+	            while (s.hasNext())
+	            {
+	               String str = s.next(); 
+	               myChar = str.toCharArray();
+	            }
+	        } finally {
+	            if (s != null) {
+	                s.close();
+	            }
+	        }
+	        for(int i = 0; i < 20; i++){
+				System.out.println(myChar[i]);
+	        }
 	}
 }
